8875bd43e1ec5e7106b381417ec297a4f2c07c98
cjungwo <jwoo516@naver.com>
2025-05-08 20:11:33 +1000

    #3-[dev] update port and add imports(ConfigModule, TypeOrmModule) in AppModule

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DB_URL: Joi.string().required(),
+        HASH_SALT: Joi.number().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.getOrThrow('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
